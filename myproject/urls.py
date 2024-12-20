2a5466011577178fa2bcbb71f49c59b26d749ea1
pvsav4enkoff <pvsav4enkoff@gmail.com>
2024-12-20 11:20:34 +0400

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,16 +19,13 @@ from django.urls import path
 from plant.views import *
 
 urlpatterns = [
-    path('', view_plant),
+    path('', view_plant), 
     path('admin/', admin.site.urls),
     path('plant/', view_plant),
-    # path('employees/', employee_list, name='employee_list'),
-    # path('plant/employee/delete/<pk>/', delete_employee, name='delete_employee'),
     path('plant/employee/edit/<pk>/', edit_employee, name='edit_employee'),
     path('plant/repair', view_plant_repair,name='repairs_list'),
     path('plant/task', view_plant_task,name='task_list'),
     path('plant/equipment', view_plant_equipment),
-    # path('plant/task', view_plant_task),
     path('plant/employee', view_plant_employee, name='employee_main'),
     path('plant/employee/sign_up', sign_up_by_plant),
     path('plant/equipment/sign_equipment', registration_equipment),
@@ -37,7 +34,6 @@ urlpatterns = [
     path('repair/complete_repair/<pk>/', complete_repair, name='complete_repair'),
     path('task/complete_task/<pk>/', complete_task, name='complete_task'),
     path('plant/task', sort_view, name='sort_view'),
-
     path('export_to_excel_task/', export_to_excel_task, name='export_to_excel_task'),
 
 
